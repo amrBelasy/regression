9207d44b2aa2116868bc992a6a9e8714869adf94
Amr Belasy <amrbelasy2@gmail.com>
2020-04-24 18:16:47 +0200

    regression

---
+++ Section 6 - Polynomial Regression/P14-Polynomial-Regression/test.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep  8 13:52:16 2019
+
+@author: amr
+"""
+
+import numpy as np
+import pandas as pd
+import matplotlib as plt
+
+dataset = pd.read_csv('Position_Salaries.csv')
